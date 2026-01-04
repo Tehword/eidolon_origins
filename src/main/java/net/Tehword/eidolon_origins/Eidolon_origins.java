8bc5fdfe8855802177f64f36ca114fc1982ef3f6
Artmax <Artzak.maz@mail.ru>
2026-01-04 15:10:36 +0300

    Ориджин :3

--- src/main/java/net/Tehword/eidolon_origins/Eidolon_origins.java
+++ src/main/java/net/Tehword/eidolon_origins/Eidolon_origins.java
@@ -1,35 +1,12 @@
 package net.Tehword.eidolon_origins;
 
-import com.mojang.logging.LogUtils;
+import net.Tehword.eidolon_origins.registries.EOcodex;
 import net.Tehword.eidolon_origins.registries.EOsigns;
 import net.Tehword.eidolon_origins.registries.EOspells;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Eidolon_origins.MODID)
@@ -41,7 +18,7 @@ public class Eidolon_origins {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         EOspells.init();
-        EOsigns.init();
+        EOsigns.init();;
         MinecraftForge.EVENT_BUS.register(this);
     }
 }
